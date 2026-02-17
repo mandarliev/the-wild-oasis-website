962317fa021bf5f08bea5df2e88f0e461818fecd
Dimitar <dimitar.mandarliev@gmail.com>
2026-02-17 21:18:01 +0200

    Fetching data in a page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,16 @@
-function Page() {
+async function Page() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users").then(
+    (response) => response.json(),
+  );
+
   return (
     <div>
       <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
