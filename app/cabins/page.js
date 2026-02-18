900ca417876aea5562dc6e921e04df2080ab30d4
Dimitar <dimitar.mandarliev@gmail.com>
2026-02-18 16:20:40 +0200

    Project organization

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,19 +1,9 @@
-import Counter from "../components/Counter";
+import Counter from "../_components/Counter";
 
-async function Page() {
-  const data = await fetch("https://jsonplaceholder.typicode.com/users").then(
-    (response) => response.json(),
-  );
-
+function Page() {
   return (
     <div>
       <h1>Cabins page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 }
