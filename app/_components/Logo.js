ad4928b6a6a06dacfd3f24415ffdbd51f803ecd3
Dimitar <dimitar.mandarliev@gmail.com>
2026-02-23 13:42:22 +0200

    Streaming route segments with loading.js file

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -5,7 +5,6 @@ import logo from "@/app/logo.png";
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />*/}
       <Image
         src={logo}
         quality={100}
--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,7 @@
+import Spinner from "./_components/Spinner";
+
 function Loading() {
-  return <p>Loading...</p>;
+  return <Spinner />;
 }
 
 export default Loading;
