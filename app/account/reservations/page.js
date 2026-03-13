c9dce8bc45b6d79aa6d8b97d13d1bd8562f0f54a
Dimitar <dimitar.mandarliev@gmail.com>
2026-03-13 13:31:01 +0200

    Deleting a reservation

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
-  const bookings = await getBookings(session.user.guestId);
+  const bookings = await getBookings(session?.user?.guestId);
 
   return (
     <div>
